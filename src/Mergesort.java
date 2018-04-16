b07ff244fc929d06ead1fe13bff806ca5a1ebbe9
KamilDudek <37578394+KamilDudek@users.noreply.github.com>
2018-04-16 23:01:36 +0200

    proba3

--- src/Mergesort.java
+++ src/Mergesort.java
@@ -1,6 +1,6 @@
 public class Mergesort {
     private static final int N = 30;
-    private static int array[] = {31,29,28,27,26,25,1,2,3,4,5,6,7,24,23,22,21,20,19,18,8,9,10,11,17,16,15,13,14,12};
+    private static int array[] = {30,29,28,27,26,25,1,2,3,4,5,6,7,24,23,22,21,20,19,18,8,9,10,11,17,16,15,13,14,12};
     private static int extraArray[] = new int[N]; //tablica do przechowywania zmiennych
 
     private static void mergeSort(int leftIndex,int rightIndex) {
