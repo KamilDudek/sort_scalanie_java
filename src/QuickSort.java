80dfb22d570aacccba659650239cb532a6f1157f
KamilDudek <37578394+KamilDudek@users.noreply.github.com>
2018-04-17 15:23:25 +0200

    quick_sort2

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,34 +1,43 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class QuickSort {
 
-    private static final int N = 30;
-    private static int array[] = {30,29,28,27,26,25,1,2,3,4,5,6,7,24,23,22,21,20,19,18,8,9,10,11,17,16,15,13,14,12};
+   private static final int N = 30;
+   private static int array[] = {30,29,28,27,26,25,1,2,3,4,5,6,7,24,23,22,21,20,19,18,8,9,10,11,17,16,15,13,14,12};
 
-    private static void quickSort(int tablica[],int leftIndex, int rightIndex){
-        if(leftIndex >= rightIndex) {
-            return;
+
+    private static void quickSort(int leftIndex, int rightIndex){
+
+        int i =  leftIndex;
+        int j = rightIndex;
+        int pivot_value =  array[leftIndex+(rightIndex-leftIndex)/2];
+
+        while(i <= j){
+            while (array[i] < pivot_value) {
+                i++;
+            }
+            while (array[j] > pivot_value) {
+                j--;
+            }
+            if (i <= j) {
+                exchangeNumbers(i, j);
+                i++;
+                j--;
+            }
         }
-        int pivot_value =  array[rightIndex];
-        int border = leftIndex-1;
-        int i = leftIndex;
+        // call quickSort() method recursively
+        if (leftIndex < j)
+            quickSort(leftIndex, j);
+        if (i < rightIndex)
+            quickSort(i, rightIndex);
+    }
 
-        while(i < rightIndex){
-            if(array[i] < pivot_value){
-                border++;
-                if(border != i) {
-
-                }
-            }
-            i++;
-            }
-            border++;
-            if(border != rightIndex){
-
-            }
-        quickSort(array,leftIndex,border-1);
-        quickSort(array,border+1,rightIndex);
-        }
+    private static void exchangeNumbers(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 
     public static void main(String[] args) {
         int i;
@@ -37,7 +46,7 @@ public class QuickSort {
         for (i=0; i<N; i++)
             System.out.print(array[i] + " ");
 
-        quickSort(array, 0,N-1);
+        quickSort(0,N-1);
 
         System.out.println("\nZbior po sortowaniu:");
         for (i=0; i<N; i++)
